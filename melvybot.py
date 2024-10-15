c1561cdc4fefb727dc132631636212832957e37d
melvy13 <melvernlim@gmail.com>
2024-10-15 12:32:27 +0800

    run bot

--- melvybot.py
+++ melvybot.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
+import discord
+from discord.ext import commands
 
 # Load bot token
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+bot.run(BOT_TOKEN)
