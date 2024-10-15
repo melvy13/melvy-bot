736e2a1b19346f49cee80a2673389cd88c824f26
melvy13 <melvernlim@gmail.com>
2024-10-16 00:56:35 +0800

    more calculator stuff

--- cogs/calculator.py
+++ cogs/calculator.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import math
 
 class Calculator(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -8,10 +9,19 @@ class Calculator(commands.Cog):
     @commands.command(description="Add numbers with each number separated by a space")
     async def add(self, ctx, *, numbers):
         string_list = numbers.split()
-        number_list = [int(i) for i in string_list]
+        number_list = [float(i) for i in string_list]
         result = sum(number_list)
 
         expression = " + ".join(string_list)
+        await ctx.send(f"{expression} = {result}")
+
+    @commands.command(description="Multiply numbers with each number separated by a space")
+    async def multiply(self, ctx, *, numbers):
+        string_list = numbers.split()
+        number_list = [float(i) for i in string_list]
+        result = math.prod(number_list)
+
+        expression = " x ".join(string_list)
         await ctx.send(f"{expression} = {result}")
 
 async def setup(bot):
