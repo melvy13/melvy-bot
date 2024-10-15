abf9f19f2ddce931d53c52ba0ae71bdae0c6ff05
melvy13 <melvernlim@gmail.com>
2024-10-15 22:49:37 +0800

    Added basic cog

---
+++ cogs/basic.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import random
+
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description="Pings the bot.....or play ping pong")
+    async def ping(self, ctx):
+        await ctx.send(f"...pong!\n\nor did you want the latency? : {(self.bot.latency*1000):.2f}ms")
+
+    @commands.command(aliases=["8ball", "eightball"], description="Roll the magic 8 ball for answering yes/no questions about your future!")
+    async def magic8ball(ctx, *, question):
+        responses = ["Yes :D",
+                 "Without a doubt :D",
+                 "Definitely yes :D",
+                 "Absolutely :D",
+                 "Certainly :D",
+                 "Most likely :)",
+                 "No :(",
+                 "Don't count on it :(",
+                 "Very doubtful :(",
+                 "Ask again later :v",
+                 "Cannot predict now :v"]
+    
+        await ctx.send(f"Your question: {question}\nBot answer: {random.choice(responses)}")
+
+def setup(bot):
+    bot.add_cog(Basic(bot))
