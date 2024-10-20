976e165191004c7f311bcb9f862ee30868d1e323
melvy13 <melvernlim@gmail.com>
2024-10-20 21:50:10 +0800

    updated clear command

--- cogs/messages.py
+++ cogs/messages.py
@@ -1,14 +1,29 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class Messages(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(description="Clear the last 6 messages (including calling command) unless specfied")
-    async def clear(self, ctx, amount=6):
-        await ctx.channel.purge(limit=amount)
-        print("Last 6 messages cleared!")
+    @commands.command(description="Clear the last few messages. Must indicate amount (number of messages to delete).")
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount):
+
+        await ctx.send(f"Are you sure you want to clear the last {amount} messages? React with 👍 to continue.")
+
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "👍"
+        
+        try:
+        # Wait for confirmation (timeout after 15 seconds)
+            await self.bot.wait_for("reaction_add", check=check, timeout=15.0)
+        except asyncio.TimeoutError:
+            await ctx.send("beep - Timed out, no messages were deleted.", delete_after=3)
+            return
+        else:
+            await ctx.channel.purge(limit=int(amount)+2) # +2 to also delete the calling message and the confirmation message
+            await ctx.send(f"Last {amount} messages have been cleared!", delete_after=3)
 
 async def setup(bot):
     await bot.add_cog(Messages(bot))
